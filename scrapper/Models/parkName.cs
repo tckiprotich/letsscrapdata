faf575d23e99c3d090377809918f5ea06f66ddbc
tckiprotich <tckiprotich@gmail.com>
2023-11-26 18:52:54 +0000

    stash

--- scrapper/Models/parkName.cs
+++ scrapper/Models/parkName.cs
@@ -10,5 +10,7 @@ namespace scrapper.Models
         // Guid
         public Guid Id {get; set;}
         public string? Name {get; set;}
+        public string? Location {get; set;}
+        
     }
 }
